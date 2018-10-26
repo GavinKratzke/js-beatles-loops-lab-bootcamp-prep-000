d2356f480f145c2fcf7d3dd27867cc84cec90fee
Gavin Kratzke <gjkkratzke@gmail.com>
2018-10-26 01:13:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 function theBeatlesPlay ( musicians, instruments) {
   var array = ["John Lennon plays guitar",]
     for (var array = 0; array <= 4; array) {
-      console.log("John Lennon plays guitar")
+      console.log("John Lennon plays guitar");
     }
 }
