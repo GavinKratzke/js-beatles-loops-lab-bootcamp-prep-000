a48e4622ba4df1846c7f6ca0f79162aeb5edbc6d
Gavin Kratzke <gjkkratzke@gmail.com>
2018-10-26 01:00:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 // add solution here
 function theBeatlesPlay ( musicians, instruments) {
-  var musicians = []
-    for (var musicians = 0; musicians <=4; musicians++ ){
-      console.log()
-    }
+  var array = []
 }
