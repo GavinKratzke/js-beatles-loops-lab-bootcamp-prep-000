eff53a320758b7da0accbf165b8bf7070b1bcd71
Gavin Kratzke <gjkkratzke@gmail.com>
2018-10-26 00:45:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 function theBeatlesPlay ( musicians, instruments) {
   var musicians = []
     for (var musicians = 0; musicians <=4; musicians++ ){
-      console.log('')
+      console.log()
     }
 }
