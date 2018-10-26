b23948f91ea276954746164de132fae0825a74b8
Gavin Kratzke <gjkkratzke@gmail.com>
2018-10-26 01:12:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // add solution here
 function theBeatlesPlay ( musicians, instruments) {
   var array = ["John Lennon plays guitar",]
-    for (var array = 0; array <= 4; )
+    for (var array = 0; array <= 4; array) {
+      console.log("John Lennon plays guitar")
+    }
 }
